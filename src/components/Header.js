81f57c5e221bd242e264669ed920c1e4a7b1473d
Dani Renner <danijrenner@gmail.com>
2021-05-04 08:23:04 -0700

    add sign in/sign out form & button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from "react";
 import thisIsAnImage from "./../img/tickets.png";
 import './../styles/Header.css';
+import { Link } from "react-router-dom";
 
 function Header(){
   return (
     <React.Fragment>
       <h1>Help Queue</h1>
       <img src={thisIsAnImage} alt="tickets" width="300px" />
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/signin">Sign In</Link>
+        </li>
+      </ul>
     </React.Fragment>
   );
 }
